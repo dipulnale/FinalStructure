4aaa90631d8d8e82e64d47accee94b4f73a455f4
dipulnale <dipulnale@gmail.com>
2021-06-02 12:45:38 +0530

    committing structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "./component/home/home.component";
+import { VegetableComponent } from "./component/vegetable/vegetable.component";
+import { LoginComponent } from "./component/login/login.component";
+import { OrderComponent } from "./component/order/order.component";
+import { BillComponent } from "./component/bill/bill.component";
+import { AdminComponent } from "./component/admin/admin.component";
+import { FeedbackComponent } from "./component/feedback/feedback.component";
+import { SignupCustomerComponent } from './component/signup-customer/signup-customer.component';
+import { SignupAdminComponent } from './component/signup-admin/signup-admin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    VegetableComponent,
+    LoginComponent,
+    OrderComponent,
+    BillComponent,
+    AdminComponent,
+    FeedbackComponent,
+    SignupCustomerComponent,
+    SignupAdminComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
